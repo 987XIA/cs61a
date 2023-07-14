095800caddabc378aa597d4c9c0b88449bb0225c
summersummer <87262365+987XIA@users.noreply.github.com>
2023-07-14 15:13:10 -0400

    lec06 qa disc02

---
+++ disc02.py
@@ -0,0 +1,101 @@
+"""""""""""Disc02"""""""""""""""
+
+# Question 1.1
+def keep_ints(cond, n):
+    """Print out all integers 1..i..n where cond(i) is true
+    
+    >>> def is_even(x):
+    ...     # Even numbers have remainder 0 when divided by 2.
+    ...     return x % 2 == 0
+    >>> keep_ints(is_even, 5)
+    2
+    4
+    """
+    for i in range (1, n+1):
+        if cond(i):
+            print(i)
+    return keep_ints
+
+
+# Question 1.2
+
+def make_keeper(n):
+    """Returns a function which takes one parameter cond and prints out
+    all integers 1..i..n where calling cond(i) returns True.
+    >>> def is_even(x):
+    ... # Even numbers have remainder 0 when divided by 2.
+    ... return x % 2 == 0
+    >>> make_keeper(5)(is_even)
+    2
+    4
+    """
+    def keep_ints(cond):
+        for i in range (1, n+1):
+            if cond(i):
+                print(i)
+        return keep_ints
+
+# Question 1.4
+
+curry2 = lambda h: lambda x: lambda y: h(x, y)
+
+make_adder = curry2(lambda x, y: x + y)
+add_three = make_adder(3)
+add_four = make_adder(4)
+five = add_three(2)
+print(five)
+
+
+# Question 1.7
+
+def print_delayed(x):
+    """Return a new function. This new function, when called,
+    will print out x and return another function with the same
+    behavior.
+    >>> f = print_delayed(1)
+    >>> f = f(2)
+    1
+    >>> f = f(3)
+    2
+    >>> f = f(4)(5)
+    3
+    4
+    >>> f("hi")
+    5
+    <function print_delayed> # a function is returned
+    """
+    def delay_print(y):
+        print(x)
+        return print_delayed(y)
+    return delay_print
+
+
+# Question 1.8
+
+def print_n(n):
+    """
+    >>> f = print_n(2)
+    >>> f = f("hi")
+    hi
+    >>> f = f("hello")
+    hello
+    >>> f = f("bye")
+    done
+    >>> g = print_n(1)
+    >>> g("first")("second")("third")
+    first
+    done
+    done
+    <function inner_print>
+    """
+    count = 0
+    def inner_print(x):
+        nonlocal count
+
+        if count >= n:
+            print("done")
+        else:
+            print(x)
+            count += 1
+        return inner_print if count < n else "done"
+    return inner_print
